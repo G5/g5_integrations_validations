52e14fbd1bb079acd068bc9a1b2fda8cd362d9c6
JP Moral <jp@aelogica.com>
2015-02-13 12:23:25 +0800

    [#88293856] Add validations for threshold options and urls for ctas on Centershift

--- lib/g5_integrations_validations/centershift_inventory_validations.rb
+++ lib/g5_integrations_validations/centershift_inventory_validations.rb
@@ -12,6 +12,7 @@ module G5IntegrationsValidations
     ]
     VALID_IN_STORE_RATE_BASES = VALID_RATE_BASES + ['calculated_from_web_rate']
     VALID_WEB_RATE_BASES = VALID_RATE_BASES + ['calculated_from_in_store_rate']
+    CTAS_THRESHOLD_OPTIONS = %w(soft_reservation hard_reservation quote_only)
 
     extend ActiveSupport::Concern
 
@@ -21,6 +22,24 @@ module G5IntegrationsValidations
         in_store_rate_bases: VALID_IN_STORE_RATE_BASES,
         web_rate_bases: VALID_WEB_RATE_BASES,
       )
+
+      validates(
+        :cta_hard_reservation_url,
+        :cta_soft_reservation_url,
+        :cta_quote_only_reservation_url,
+        presence: true
+      )
+
+      validates :unit_availability_threshold, presence: true
+      validates(:unit_availability_cta_in_and_above_threshold, {
+        presence: true,
+        inclusion: {in: CTAS_THRESHOLD_OPTIONS}
+      })
+      validates(:unit_availability_cta_below_threshold, {
+        inclusion: {in: CTAS_THRESHOLD_OPTIONS},
+        allow_blank: true
+      })
+
     end
   end
 end
--- spec/g5_integrations_validations/centershift_inventory_validations_spec.rb
+++ spec/g5_integrations_validations/centershift_inventory_validations_spec.rb
@@ -7,6 +7,23 @@ module G5IntegrationsValidations
 
     it { is_expected.to validate_presence_of(:in_store_rate_basis) }
     it { is_expected.to validate_presence_of(:web_rate_basis) }
+    it { is_expected.to validate_presence_of(:cta_hard_reservation_url) }
+    it { is_expected.to validate_presence_of(:cta_soft_reservation_url) }
+    it { is_expected.to validate_presence_of(:cta_quote_only_reservation_url) }
 
+    it do
+      is_expected.
+        to validate_presence_of(:unit_availability_cta_in_and_above_threshold)
+    end
+    it do
+      is_expected.
+        to validate_inclusion_of(:unit_availability_cta_in_and_above_threshold).
+        in_array(%w(soft_reservation hard_reservation quote_only))
+    end
+    it do
+      is_expected.
+        to validate_inclusion_of(:unit_availability_cta_below_threshold).
+        in_array(%w(soft_reservation hard_reservation quote_only))
+    end
   end
 end
