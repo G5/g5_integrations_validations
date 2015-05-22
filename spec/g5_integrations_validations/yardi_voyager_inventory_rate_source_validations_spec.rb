53124a58718277f0f224178a62e01ee11cb8482d
JP Moral <jp@aelogica.com>
2015-05-22 08:21:50 +0800

    Fix failing specs, move specs into their own modules where necessary

---
+++ spec/g5_integrations_validations/yardi_voyager_inventory_rate_source_validations_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+module G5IntegrationsValidations
+  describe YardiVoyagerInventoryRateSourceValidations do
+
+    subject { YardiVoyagerInventory.new }
+
+    it do
+      is_expected.
+        to validate_inclusion_of(:rate_source).
+        in_array(%w(vendor_rate available_rate))
+    end
+
+  end
+end
